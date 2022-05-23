27df2482fe93b4fd2edbf2441a919cd29f47d325
roncamposj <42298486+roncamposj@users.noreply.github.com>
2022-05-23 19:58:03 -0400

    Add files via upload

--- routes/collections.js
+++ routes/collections.js
@@ -1,1 +1,35 @@
+const express = require('express')
+const router = express.Router()
+const Project = require('../models/projects.js')
+const Stationary_Collection = require('../models/stationary_collections.js')
+const Moving_Collection = require('../models/moving_collections.js')
+const Survey_Collection = require('../models/survey_collections.js')
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
+const { models } = require('mongoose')
 
+const { BadRequestError, UnauthorizedError } = require('../utils/errors')
+
+
+router.get('/moving/:id', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
+    res.status(200).json(await Moving_Collection.findById(req.params.id)
+                                                .populate('area')
+                                                .populate('maps'))
+
+})
+
+router.get('/stationary/:id', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
+    res.status(200).json(await Stationary_Collection.findById(req.params.id)
+                                                .populate('area')
+                                                .populate('maps'))
+
+})
+router.get('/survey/:id', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
+    res.status(200).json(await Survey_Collection.findById(req.params.id)
+                                                .populate('area')
+                                                .populate('surveys'))
+
+})
+
+module.exports = router
