6dbc4916c6b0d28bcccb4595b264b7184111b427
Whisel <informer444@gmail.com>
2022-06-07 18:37:12 -0400

    added polyfills react combo

--- frontend_web/src/index.js
+++ frontend_web/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './polyfills'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
