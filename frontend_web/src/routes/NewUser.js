b2cc595701aa5fd118b444880cea956c4a7a12d2
Whisel <informer444@gmail.com>
2022-06-06 23:29:54 -0400

    Revert "test"
    This reverts commit 301b96cde74753c67b31aac6f3eea773b305a723.

--- frontend_web/src/routes/NewUser.js
+++ frontend_web/src/routes/NewUser.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { router } from 'express';
 import Card from 'react-bootstrap/Card';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
