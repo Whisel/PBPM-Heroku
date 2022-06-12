f108217d22c35676e5adfd43b780fe206a07eba9
Whisel <informer444@gmail.com>
2022-06-12 18:29:20 -0400

    Changes to axios.js and package.json for baseURL

--- frontend_web/src/api/axios.js
+++ frontend_web/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://measuringplacesd2.herokuapp.com/api'
+    baseURL: 'https://measuringplacesd.herokuapp.com/api'
 });
